ce03ab1b8fab755526649f499062124cb8c6fb50
Narotam-Mishra <narotam21jan@gmail.com>
2024-02-28 15:30:55 +0530

    react-faq_1.3_dbs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import DynamicSearchFilter from "./components/DynamicSearchFilter"
+// import DynamicSearchFilter from "./components/DynamicSearchFilter"
 import './App.css';
+import Debouncing from './components/Debouncing';
 
 function App() {
 
   return (
     <>
-      <DynamicSearchFilter />
+      {/* <DynamicSearchFilter /> */}
+      <Debouncing />
     </>
   )
 }
---
+++ src/components/Debouncing.jsx
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+import '../styles/utility.css';
+
+const Debouncing = () => {
+  const[pin, setPin] = useState();
+  const pinUrl = `https://api.postalpincode.in/pincode/`;
+
+  useEffect(() => {
+    const debouncedASearch = setTimeout(() => {
+        axios.get(pinUrl + pin).then((res) => {
+            console.log(res.data[0].PostOffice);
+          })
+          .catch((err) => {
+              console.log("Something went wrong:",err);
+          })
+    },1500)
+
+    return() => {
+        clearTimeout(debouncedASearch);
+    }
+  }, [pin, pinUrl])
+
+  return (
+    <div className="con">
+      <input
+        type="text"
+        onChange={(e) => setPin(e.target.value)}
+        placeholder="Enter your Pincode"
+      />
+    </div>
+  );
+}
+
+export default Debouncing
